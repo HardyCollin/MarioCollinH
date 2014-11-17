8ca0221de15b850acf9e0d797f62630e866a81e0
Collin hardy <Collin.Hardy25@gmail.com>
2014-11-17 12:11:32 -0800

    edits to mario

--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -4,8 +4,10 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
-
-	/* Atlases 
+        {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
+        {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
+	{name: "Mario", type:"image", src: "data/img/player1.png"},
+        /* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
 	 */
@@ -15,8 +17,9 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
-
-	/* Background music. 
+        {name: "level04", type: "tmx", src: "data/map/level04.tmx"}
+	
+        /* Background music. 
 	 * @example
 	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
 	 */	
